f6517c5c6fa9b059ba6d5b0a63a875857885cd44
creativebcx <ericdonley1@gmail.com>
2017-04-17 19:10:42 -0600

    fully-operating

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-//totalWordCount(["aaaa", "z", "zzzzz", "z", "1", "y"]);
-//var textInTheBox = $('#user-text').val();
-
 $('.js-button').click(function(event) {
 	event.preventDefault();
 	//prevents default behavior of the button we are clicking
 	var textInTheBox = $('#user-text').val();
+	//what is val()?
 	var textArray = textInTheBox.split(/[ "().+,?]/).filter(Boolean);
-	console.log(totalWordCount(textArray));
-	console.log(uniqueWordCount(textArray));
-	console.log(avgWordLength(textArray));
+	$('.hidden').removeClass('hidden');
+	$('.js-total').append(totalWordCount(textArray));
+	$('.js-unique').append(uniqueWordCount(textArray));
+	$('.js-avg').append(avgWordLength(textArray));
+	//console.log(totalWordCount(textArray));
+	//console.log(uniqueWordCount(textArray));
+	//console.log(avgWordLength(textArray));
 	});
+
 
 
 function totalWordCount(text) {
 	var measureTotalIndexNum = [];
 	for (var i=0; i<text.length; i++) {
 	}
-	return "Word count: " + i;
+	return i;
 }
 
 function uniqueWordCount(text) {
